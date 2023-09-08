ef373af3213bc979c346262c044c3bab23309ab1
WCHLi <791878743@qq.com>
2023-09-08 16:33:09 +0900

    no message

---
+++ mybatisplus_02_dql/src/main/java/com/itheima/Mybatisplus02DqlApplication.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import com.itheima.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatisplus02DqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatisplus02DqlApplication.class, args);
+    }
+
+}
